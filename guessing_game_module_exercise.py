dba82b4951cf69a48ec34128230a91ce37dae31c
Magnificent Su <magnificentsu@MacBook-Pro-3.local>
2023-12-25 06:20:25 -0600

    Lessons from 25 Dec 2023

---
+++ guessing_game_module_exercise.py
@@ -0,0 +1,21 @@
+import sys
+import random
+
+
+random_number = random.randint(int(sys.argv[1]), int(sys.argv[2]))
+
+while True:
+    try:
+        print(random_number)
+        guessed_number = int(input('Guess a number between 1 and 10:   '))
+        if 0 < guessed_number < 11:
+            if guessed_number == random_number:
+                print('You\'re a Genius')
+                break
+        else:
+            print('hey playa, between 1 and 10 please')
+    except ValueError:
+        print('Please enter any number from 1 to 10')
+
+
+
---
+++ sys_module_1.py
@@ -0,0 +1,9 @@
+import sys
+
+first = sys.argv[1]
+last = sys.argv[2]
+
+print(f'hiiiiiii! {first} {last}')
+
+# Rest of the modifications done in command terminal.
+# One of the advantages/functionality proivded to us with the sys module.
